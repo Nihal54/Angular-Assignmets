350278ccec8caad2432341d09e0645047542bd31
Nihal54 <117541272+Nihal54@users.noreply.github.com>
2023-04-13 13:17:52 +0530

    Add files via upload

---
+++ src/app/new-comp/new-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-comp',
+  template:`
+  <input type="text" style="background-color: black; color:red;">
+  `
+
+})
+export class NewCompComponent {
+
+public Name = "Marvellous";
+public Name2 = "Infosystems"
+
+}
